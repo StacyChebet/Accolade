869ca756422de05c0c11a0ac9d4fd03b3f829630
StacyChebet <staceychebet23@gmail.com>
2019-01-05 00:49:55 +0300

    Add vew function to render search results page

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
 import datetime as dt
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='/accounts/login')
+def search(request):
+    return render(request, 'search.html')
+
+
+
     
