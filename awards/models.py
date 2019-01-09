151c17d54f069dd25f416a2182420217db8e17b1
StacyChebet <staceychebet23@gmail.com>
2019-01-09 10:40:28 +0300

    Create the project model

--- awards/models.py
+++ awards/models.py
@@ -6,4 +6,17 @@ class Profile(models.Model):
     awardee = models.ForeignKey(User, on_delete=models.CASCADE)
     prof_pic = models.ImageField()
     Bio = models.TextField()
+
+class Project(models.Model):
+    awardee = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/')
+    title = models.CharField(max_length=40)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+
+    def save_project(self):
+        self.save()
     
+    @classmethod
+    def get_all_projects(self):
+        projects = Project.objects.all()
+        return projects
